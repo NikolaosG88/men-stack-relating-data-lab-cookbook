1eddd85852f3ceb82731097990473ccd945d0704
NikolaosG <Nikolaos_Gkazgkas@outlook.com>
2024-10-28 15:09:54 +0000

    cookbook

--- models/user.js
+++ models/user.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
 
+
+const foodSchema = new mongoose.Schema({
+  dish: String,
+  description: String,
+  status: Boolean
+  price: Number,
+});
+
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +18,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  pantry:[foodSchema]
 });
 
 const User = mongoose.model('User', userSchema);
